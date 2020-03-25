184269c8d9a8ed4cdd728de10330faeb51344da5
Bartosz Przydatek <bartoszprzydatek1995@gmail.com>
2020-03-25 17:36:47 +0100

    Add additional operation types: cash withdrawal & commission

--- src/main/java/operationtype/PKOOperationTypeResolver.java
+++ src/main/java/operationtype/PKOOperationTypeResolver.java
@@ -6,18 +6,32 @@ import static java.util.logging.Logger.GLOBAL_LOGGER_NAME;
 import static java.util.logging.Logger.getLogger;
 import static utils.Constants.*;
 
-public class PKOOperationTypeResolver implements OperationTypeResolver {
-    private final static Logger LOGGER = getLogger(GLOBAL_LOGGER_NAME);
+
+public class PKOOperationTypeResolver
+    implements OperationTypeResolver
+{
+    private final static Logger LOGGER = getLogger( GLOBAL_LOGGER_NAME );
+
 
     @Override
-    public OperationType resolve(String typeOperationDesc) {
-        if (typeOperationDesc.startsWith( TRANSFER_PL ) ) {
-            if (typeOperationDesc.contains(TRANSFER_TYPE_INCOME_SHORT_PL)) return OperationType.INCOME_TRANSFER;
-            else if (typeOperationDesc.contains(TRANSFER_TYPE_OUTGOING_PL)) return OperationType.OUTGOING_TRANSFER;
+    public OperationType resolve( String typeOperationDesc )
+    {
+        if( typeOperationDesc.startsWith( TRANSFER_PL ) )
+        {
+            if( typeOperationDesc.contains( TRANSFER_TYPE_INCOME_SHORT_PL ) )
+                return OperationType.INCOME_TRANSFER;
+            else if( typeOperationDesc.contains( TRANSFER_TYPE_OUTGOING_PL ) )
+                return OperationType.OUTGOING_TRANSFER;
         }
-        else if ( typeOperationDesc.contains( DEBIT_CARD_PAYMENT_RESOLVER_TXT_PL ) ) return OperationType.DEBIT_CARD_PAYMENT;
-        else if ( typeOperationDesc.contains( MOBILE_CODE_PAYMENT_RESOLVER_TXT_PL ) ) return OperationType.MOBILE_CODE_PAYMENT;
-        LOGGER.warning("Operation type can not be resolved");
+        else if( typeOperationDesc.contains( DEBIT_CARD_PAYMENT_RESOLVER_TXT_PL ) )
+            return OperationType.DEBIT_CARD_PAYMENT;
+        else if( typeOperationDesc.contains( MOBILE_CODE_PAYMENT_RESOLVER_TXT_PL ) )
+            return OperationType.MOBILE_CODE_PAYMENT;
+        else if( typeOperationDesc.contains( CASH_WITHDRAWAL ) )
+            return OperationType.CASH_WITHDRAWAL;
+        else if( typeOperationDesc.contains( COMMISSION ) )
+            return OperationType.COMMISSION;
+        LOGGER.warning( "Operation type can not be resolved" );
         return OperationType.NOT_RESOLVED;
     }
 }
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -4,18 +4,22 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 
-public class Constants {
+
+public class Constants
+{
     public static String APP_TITLE = "BSR - Bank Statement Reader";
     public static String SELECT_BANK_TXT = "Select bank";
     public static String SELECT_BANK_STATEMENT_TXT = "Select bank statement";
 
-    public static List BANK_STMT_ALLOWED_EXTENSIONS = asList(".pdf");
+    public static List BANK_STMT_ALLOWED_EXTENSIONS = asList( ".pdf" );
 
     public static String TRANSFER_PL = "PRZELEW";
     public static String TRANSFER_TYPE_INCOME_SHORT_PL = "PRZYCH";
     public static String TRANSFER_TYPE_OUTGOING_PL = "WYCHODZĄCY";
     public static String DEBIT_CARD_PAYMENT_RESOLVER_TXT_PL = "ZAKUP PRZY UŻYCIU KARTY";
     public static String MOBILE_CODE_PAYMENT_RESOLVER_TXT_PL = "KOD MOBILNY";
+    public static String CASH_WITHDRAWAL = "WYPŁATA W BANKOMACIE";
+    public static String COMMISSION = "PROWIZJA";
 
     public static String PKO = "PKO";
     public static String PKO_BANK_STMT_BALANCE_SUMMARY_TXT = "Saldo końcowe";
@@ -23,7 +27,6 @@ public class Constants {
     public static String PKO_DATE_FORMAT = "dd.MM.yyyy";
 
     public static String REGEX_AMOUNT = "-?[0-9 ]+,\\d{2}";
-
 
     public static char DOT = '.';
 }

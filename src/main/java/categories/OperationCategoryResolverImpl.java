aabb71791a5b6213857895e99da2680cf822071a
Bartosz Przydatek <bartoszprzydatek1995@gmail.com>
2020-03-27 21:44:31 +0100

    Add OperationCategoryResolverTest and achieve 100% coverage

--- src/main/java/categories/OperationCategoryResolverImpl.java
+++ src/main/java/categories/OperationCategoryResolverImpl.java
@@ -3,14 +3,19 @@ package categories;
 import model.Category;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
+import static java.util.logging.Logger.GLOBAL_LOGGER_NAME;
+import static java.util.logging.Logger.getLogger;
 import static model.Category.OTHER_CATEGORY;
 
 
 public class OperationCategoryResolverImpl
     implements OperationCategoryResolver
 {
-    Category[] categories;
+    private final static Logger LOGGER = getLogger( GLOBAL_LOGGER_NAME );
+
+    private Category[] categories;
 
     public OperationCategoryResolverImpl( Category[] categories ){
         this.categories = categories;
@@ -19,6 +24,10 @@ public class OperationCategoryResolverImpl
     @Override
     public Category resolve( String operationDescription )
     {
+        if( operationDescription == null || operationDescription.isEmpty() ){
+            LOGGER.info( "Operation description is null or empty" );
+            return OTHER_CATEGORY;
+        }
         String descriptionCaseLowered = operationDescription.toLowerCase();
         for( Category category : categories )
         {
---
+++ src/test/java/categories/OperationCategoryResolverTest.java
@@ -0,0 +1,68 @@
+package categories;
+
+import model.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class OperationCategoryResolverTest
+{
+    @Test
+    public void should_Return_Other_Category_When_Null_As_Desc_Given(){
+        //GIVEN
+        OperationCategoryResolver operationCategoryResolver = new OperationCategoryResolverImpl( null );
+
+        //WHEN
+        Category resolvedCategory = operationCategoryResolver.resolve( null );
+
+        //THEN
+        assertEquals(Category.OTHER_CATEGORY, resolvedCategory);
+    }
+
+    @Test
+    public void should_Return_Other_Category_When_Empty_String_As_Desc_Given(){
+        //GIVEN
+        OperationCategoryResolver operationCategoryResolver = new OperationCategoryResolverImpl( null );
+
+        //WHEN
+        Category resolvedCategory = operationCategoryResolver.resolve( "" );
+
+        //THEN
+        assertEquals(Category.OTHER_CATEGORY, resolvedCategory);
+    }
+
+    @Test
+    public void should_Return_Other_Category_When_Cannot_Match_Given_Desc_To_Any_Keyword(){
+        //GIVEN
+        OperationCategoryResolver operationCategoryResolver =
+            new OperationCategoryResolverImpl( createCategoriesMock() );
+
+        //WHEN
+        Category resolvedCategory = operationCategoryResolver.resolve( "NOT_MATCHING_KEYWORD" );
+
+        //THEN
+        assertEquals(Category.OTHER_CATEGORY, resolvedCategory);
+    }
+
+    @Test
+    public void should_Return_Category_When_Can_Match_Given_Desc_To_Some_Keyword(){
+        //GIVEN
+        OperationCategoryResolver operationCategoryResolver =
+            new OperationCategoryResolverImpl( createCategoriesMock() );
+
+        //WHEN
+        Category resolvedCategory = operationCategoryResolver.resolve( "qwert" );
+
+        //THEN
+        assertEquals(createCategoryMock().getCategoryName(), resolvedCategory.getCategoryName());
+    }
+
+    private Category[] createCategoriesMock(){
+        return new Category[]{createCategoryMock()};
+    }
+
+    private Category createCategoryMock(){
+        return new Category( "MOCK_CATEGORY", new String[]{"qwert", "asdfg", "zxcvb"} );
+    }
+}

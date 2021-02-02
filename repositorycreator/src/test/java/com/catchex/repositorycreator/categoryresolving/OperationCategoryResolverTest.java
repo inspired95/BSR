b26af63375efebafcb82326c5c87ef371c34d1e9
Bartosz Przydatek <bartoszprzydatek1995@gmail.com>
2021-02-02 22:03:49 +0100

    OperationCategoryResolver unit tests added

--- repositorycreator/src/test/java/com/catchex/repositorycreator/categoryresolving/OperationCategoryResolverTest.java
+++ repositorycreator/src/test/java/com/catchex/repositorycreator/categoryresolving/OperationCategoryResolverTest.java
@@ -1,77 +1,83 @@
 package com.catchex.repositorycreator.categoryresolving;
 
-public class OperationCategoryResolverTest
-{/*
-    @Test
-    public void should_Return_Other_Category_When_Null_As_Desc_Given()
+import com.catchex.models.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+import static com.catchex.logging.Log.initLogging;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class OperationCategoryResolverTest
+{
+    private final String categoryName = "CAR";
+    private final String categoryKeyword = "fuel";
+
+
+    @BeforeAll
+    public static void setUpBeforeAll()
     {
-        //GIVEN
-        OperationCategoryResolver operationCategoryResolver = new OperationCategoryResolverImpl();
-
-        //WHEN
-        Category resolvedCategory = operationCategoryResolver.resolve( null );
-
-        //THEN
-        assertEquals( Category.OTHER_CATEGORY, resolvedCategory );
+        initLogging();
     }
 
 
-    @Test
-    public void should_Return_Other_Category_When_Empty_String_As_Desc_Given()
+    private static Stream<Arguments> provideDescriptionsTypesAndExpectedResults()
     {
-        //GIVEN
-        OperationCategoryResolver operationCategoryResolver = new OperationCategoryResolverImpl();
-
-        //WHEN
-        Category resolvedCategory = operationCategoryResolver.resolve( "" );
-
-        //THEN
-        assertEquals( Category.OTHER_CATEGORY, resolvedCategory );
+        return Stream.of(
+            Arguments.of( "", null, Category.OTHER_CATEGORY ),
+            Arguments.of( null, OperationType.COMMISSION, Category.OTHER_CATEGORY ),
+            Arguments.of( "", OperationType.COMMISSION, Category.OTHER_CATEGORY ),
+            Arguments.of( "", OperationType.CASH_WITHDRAWAL, Category.CASH_WITHDRAWAL ),
+            Arguments.of( "NOT_MATCHING", OperationType.COMMISSION, Category.OTHER_CATEGORY ) );
     }
 
 
-    @Test
-    public void should_Return_Other_Category_When_Cannot_Match_Given_Desc_To_Any_Keyword()
-    {
-        //GIVEN
-        OperationCategoryResolver operationCategoryResolver = new OperationCategoryResolverImpl();
-
-        //WHEN
-        Category resolvedCategory = operationCategoryResolver.resolve( "NOT_MATCHING_KEYWORD" );
-
-        //THEN
-        assertEquals( Category.OTHER_CATEGORY, resolvedCategory );
-    }
-
-
-    @Test
-    public void should_Return_Category_When_Can_Match_Given_Desc_To_Some_Keyword()
-    {
-        //GIVEN
-        OperationCategoryResolver operationCategoryResolver = new OperationCategoryResolverImpl();
-
-        //WHEN
-        Category resolvedCategory = operationCategoryResolver.resolve( "qwert" );
-
-        //THEN
-        assertEquals( createCategoryMock().getCategoryName(), resolvedCategory.getCategoryName() );
-    }
-
-
-    private SortedSet<Category> createCategoriesMock()
-    {
-        SortedSet<Category> categories = new TreeSet<>();
-        categories.add( createCategoryMock() );
-        return categories;
-    }
-
-
-    private Category createCategoryMock()
+    private void setupConfiguration()
     {
         SortedSet<Keyword> keywords = new TreeSet<>();
-        keywords.add( new Keyword( "qwert" ) );
-        keywords.add( new Keyword( "asdfg" ) );
-        keywords.add( new Keyword( "zxcvb" ) );
-        return new Category( "MOCK_CATEGORY", keywords );
-    }*/
+        keywords.add( new Keyword( categoryKeyword ) );
+
+        Category carCategory = new Category( categoryName, keywords );
+
+        SortedSet<Category> categories = new TreeSet<>();
+        categories.add( carCategory );
+
+        CategoriesConfiguration categoriesConfiguration = new CategoriesConfiguration( categories );
+
+        Configuration.getInstance().setCategoriesConfiguration( categoriesConfiguration );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource( "provideDescriptionsTypesAndExpectedResults" )
+    void should_Return_Other_Category_When_Null_As_Type_Given(
+        String description, OperationType type, Category expectedCategory )
+    {
+        //WHEN
+        Category resolvedCategory = OperationCategoryResolver.resolve( description, type );
+
+        //THEN
+        assertEquals( expectedCategory, resolvedCategory );
+    }
+
+
+    @Test
+    void should_Return_Proper_Category_When_Can_Match_Given_Desc_To_Some_Keyword()
+    {
+        //GIVEN
+        setupConfiguration();
+
+        //WHEN
+        Category resolvedCategory = OperationCategoryResolver.resolve( categoryKeyword );
+
+        //THEN
+        assertEquals( categoryName, resolvedCategory.getCategoryName() );
+    }
 }
